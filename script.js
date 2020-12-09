4d688d2ccbe67755d089d7662ea89c751240420a
Yolanda <ymoretto@gmail.com>
2020-12-09 15:28:58 -0300

    finalizado workMenu interativo com js

---
+++ script.js
@@ -0,0 +1,20 @@
+const workMenu = document.querySelectorAll('.js-titlebox h2');
+const workContent = document.querySelectorAll('.js-work__menu .js-item__portfolio');
+
+//checking if the items exist before calling the functions
+if(workMenu.length && workContent.length) {
+    //toggle the active class in the js-item__portfolio//
+    const activateWork = (index) => {
+        workContent.forEach((content) => {
+            content.classList.remove('active');
+        })
+        workContent[index].classList.add('active');
+    };
+
+    workMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activateWork(index);
+        });
+    });
+}
+
